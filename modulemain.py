600fc91511a248944eb97ee85ef6b87eb53fba20
CalosChen <359000081@qq.com>
2018-09-09 18:29:08 +0800

    python class and normal data operations

--- modulemain.py
+++ modulemain.py
@@ -4,10 +4,10 @@ import module1
 import sys,os,re,pymysql
 module1.hello()
 
-greeting=input()
-name=sys.stdin.readline()
-print('%s%s'%(greeting,name,))
-sys.stdout.write(name)
+# greeting=input()
+# name=sys.stdin.readline()
+# print('%s%s'%(greeting,name,))
+# sys.stdout.write(name)
 arg1=sys.argv[1]
 print(arg1)
 
@@ -18,3 +18,31 @@ rs=cursor.fetchall()
 for row in rs:
     print('%s %s'%(row[0],row[1],))
 db.close()
+
+aset=(1,2,3,6,5,)
+adict={'a':5,'b':3}
+alist=[1,2,4,5]
+for el in aset:
+    print(el)
+for el in adict:
+    print(adict[el])
+
+try:
+    print(1)
+except:
+    print(2)
+finally:
+    print('final')
+i=5
+while i>2:
+    print(i)
+    i=i-1
+print('looped')
+
+class Base:
+    def __init__(self,id):
+        self.id=id
+    def show(self):
+        print(self.id)
+base=Base(2)
+base.show()
