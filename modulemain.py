90c02942e3516e4d2f20472afac9e81ae8cf48ff
CalosChen <359000081@qq.com>
2018-09-09 17:48:26 +0800

    add python mysqlli relevant

--- modulemain.py
+++ modulemain.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import module1
-import sys,os,re
+import sys,os,re,pymysql
 module1.hello()
 
 greeting=input()
@@ -10,3 +10,11 @@ print('%s%s'%(greeting,name,))
 sys.stdout.write(name)
 arg1=sys.argv[1]
 print(arg1)
+
+db=pymysql.connect('localhost','root','cst','test')
+cursor=db.cursor()
+cursor.execute('select * from table1')
+rs=cursor.fetchall()
+for row in rs:
+    print('%s %s'%(row[0],row[1],))
+db.close()
