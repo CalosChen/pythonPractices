8852ef91e5ecc47f1186ea6fca88080e6ed7737a
CalosChen <359000081@qq.com>
2018-09-09 17:41:58 +0800

    add python sys and argv input interaction methods

--- modulemain.py
+++ modulemain.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
 import module1
-
+import sys,os,re
 module1.hello()
+
+greeting=input()
+name=sys.stdin.readline()
+print('%s%s'%(greeting,name,))
+sys.stdout.write(name)
+arg1=sys.argv[1]
+print(arg1)
